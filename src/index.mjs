45f1a6626e6ba9446b5d90a802a7cd0a237fd8d8
Yosuke Torii <jinjorweb@gmail.com>
2022-02-01 23:11:42 +0900

    introduce RoomManager

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,3 @@
-//  Durable Objects 利用のためにはこの新しい mjs な syntax が必要
-// その他の例：
-//   * https://github.com/cloudflare/durable-objects-template
-//   * https://github.com/cloudflare/durable-objects-rollup-esm
-//   * https://github.com/cloudflare/durable-objects-webpack-commonjs
-
-// 新しい syntax では global 変数の代わりに env を使うとのこと
-// 設定の仕方は wrangler.toml 参照のこと
-
-// ArrayBuffer として import できるように wrangler.toml で設定している
-// （アセットサイズには上限があるため、大きなサイトは Workers KV を使った方が良いとのこと）
 import HTML from "index.html";
 
 // エラーを 500 にする
@@ -32,13 +21,11 @@ async function handleErrors(request, func) {
   }
 }
 
-// 旧式： `addEventHandler("fetch", event => { ... })`;
-// cron 起点の時は `scheduled` でハンドルするらしい
 export default {
   async fetch(request, env) {
     return await handleErrors(request, async () => {
-      let url = new URL(request.url);
-      let path = url.pathname.slice(1).split("/");
+      const url = new URL(request.url);
+      const path = url.pathname.slice(1).split("/");
 
       if (!path[0]) {
         // Serve our HTML at the root path.
@@ -69,8 +56,13 @@ async function handleApiRequest(path, request, env) {
         if (request.method == "POST") {
           // POST /api/rooms
 
+          // TODO: ここでアクティブな部屋数の上限に達していたら 403
+
           // room 名前空間内で一意な ID
-          let id = env.rooms.newUniqueId();
+          const id = env.rooms.newUniqueId();
+
+          // TODO: ここで managed rooms に id を登録する
+
           return new Response(id.toString(), {
             headers: { "Access-Control-Allow-Origin": "*" },
           });
@@ -80,45 +72,25 @@ async function handleApiRequest(path, request, env) {
         }
       }
 
-      // OK, the request is for `/api/room/<name>/...`. It's time to route to the Durable Object
-      // for the specific room.
-      let name = path[1];
-
-      // Each Durable Object has a 256-bit unique ID. IDs can be derived from string names, or
-      // chosen randomly by the system.
-      let id;
-      if (name.match(/^[0-9a-f]{64}$/)) {
-        // The name is 64 hex digits, so let's assume it actually just encodes an ID. We use this
-        // for private rooms. `idFromString()` simply parses the text as a hex encoding of the raw
-        // ID (and verifies that this is a valid ID for this namespace).
-        id = env.rooms.idFromString(name);
-      } else if (name.length <= 32) {
-        // Treat as a string room name (limited to 32 characters). `idFromName()` consistently
-        // derives an ID from a string.
-        id = env.rooms.idFromName(name);
-      } else {
-        return new Response("Name too long", { status: 404 });
+      // `/api/room/<name>/...`
+      const name = path[1];
+      if (!name.match(/^[0-9a-f]{64}$/)) {
+        return new Response("Invalid room name", { status: 400 });
       }
+      const id = env.rooms.idFromString(name);
+
+      // TODO: ここで id を作成済みであることを確認する必要がある
+      // managed rooms に存在しない場合は 404
+
       console.log("name: " + name);
       console.log("id: " + id);
 
-      // Get the Durable Object stub for this room! The stub is a client object that can be used
-      // to send messages to the remote Durable Object instance. The stub is returned immediately;
-      // there is no need to await it. This is important because you would not want to wait for
-      // a network round trip before you could start sending requests. Since Durable Objects are
-      // created on-demand when the ID is first used, there's nothing to wait for anyway; we know
-      // an object will be available somewhere to receive our requests.
       // スタブ（クライアント）が即時に作られる。リモートでは ID が最初に使われた時に必要に応じて作られる。
-      let roomObject = env.rooms.get(id);
+      const roomObject = env.rooms.get(id);
 
-      // Compute a new URL with `/api/room/<name>` removed. We'll forward the rest of the path
-      // to the Durable Object.
-      let newUrl = new URL(request.url);
+      // `/api/room/<name>/...` の ... 部分
+      const newUrl = new URL(request.url);
       newUrl.pathname = "/" + path.slice(2).join("/");
-
-      // Send the request to the object. The `fetch()` method of a Durable Object stub has the
-      // same signature as the global `fetch()` function, but the request is always sent to the
-      // object, regardless of the request's URL.
       return roomObject.fetch(newUrl, request);
     }
 
@@ -128,11 +100,35 @@ async function handleApiRequest(path, request, env) {
 }
 
 // =======================================================================================
+// The RoomManager Durable Object Class
+
+export class RoomManager {
+  constructor(controller, env) {
+    this.storage = controller.storage;
+    this.env = env;
+  }
+
+  async fetch(request) {
+    return await handleErrors(request, async () => {
+      const url = new URL(request.url);
+      console.log("RoomManager's fetch(): " + request.method, request.url);
+
+      switch (url.pathname) {
+        case "/list": {
+          // TODO: implement
+          return new Response("Not found", { status: 404 });
+        }
+        default: {
+          return new Response("Not found", { status: 404 });
+        }
+      }
+    });
+  }
+}
+
+// =======================================================================================
 // The ChatRoom Durable Object Class
 
-// ChatRoom implements a Durable Object that coordinates an individual chat room. Participants
-// connect to the room using WebSockets, and the room broadcasts messages from each participant
-// to all others.
 export class ChatRoom {
   constructor(controller, env) {
     // get()/put() を持つ Durable Storage
@@ -142,29 +138,23 @@ export class ChatRoom {
     // 各クライアントの WebSocket object とメタデータ
     this.sessions = [];
 
-    // We keep track of the last-seen message's timestamp just so that we can assign monotonically
-    // increasing timestamps even if multiple messages arrive simultaneously (see below).
     // 同時にメッセージが来てもタイムスタンプを単調増加にするための仕掛け
     this.lastTimestamp = 0;
   }
 
-  // ここに Worker からのリクエストが来る。インターネットから直接は来ない。
-  // 今のところフォーマットは HTTP としている
   async fetch(request) {
     return await handleErrors(request, async () => {
-      let url = new URL(request.url);
-      console.log("ChatRoom's fetch(): " + request.method);
-      console.log("ChatRoom's fetch(): " + request.url);
+      const url = new URL(request.url);
+      console.log("ChatRoom's fetch(): " + request.method, request.url);
 
       switch (url.pathname) {
         case "/websocket": {
-          // `/api/room/<name>/websocket` WebSocket セッションを確立
           if (request.headers.get("Upgrade") != "websocket") {
             return new Response("expected websocket", { status: 400 });
           }
 
           // Get the client's IP address for use with the rate limiter.
-          let ip = request.headers.get("CF-Connecting-IP");
+          const ip = request.headers.get("CF-Connecting-IP");
 
           // To accept the WebSocket request, we create a WebSocketPair (which is like a socketpair,
           // i.e. two WebSockets that talk to each other), we return one end of the pair in the
@@ -172,7 +162,7 @@ export class ChatRoom {
           // Fetch API standard; unfortunately, the Fetch API / Service Workers specs do not define
           // any way to act as a WebSocket server today.
           // https://developers.cloudflare.com/workers/runtime-apis/websockets
-          let pair = new WebSocketPair();
+          const pair = new WebSocketPair();
 
           // We're going to take pair[1] as our end, and return pair[0] to the client.
           await this.handleSession(pair[1], ip);
